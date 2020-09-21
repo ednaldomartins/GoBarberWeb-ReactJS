73d9df19e19a06d290690d07c429f17e4be4ca1a
Ednaldo <martinsednaldo@outlook.com>
2020-09-21 17:03:35 -0300

    Layout inicial da autenticação.

---
+++ src/page/_layout/auth/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+import {Wrapper} from './style'
+
+export default function AuthLayout({children}) {
+  return (
+    <Wrapper>
+      {children}
+    </Wrapper>
+  );
+}
+
+AuthLayout.propTypes = {
+  children: PropTypes.element.isRequired,
+}
---
+++ src/page/_layout/auth/style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  height: 100%;
+  background: linear-gradient(-90deg, #ff9800, #c66900);
+`
+
+/*
+color Primary: #ff9800
+color Dark: #c66900
+color Light: #ffc947
+*/
