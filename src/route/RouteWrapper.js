3ba33f78fdfb147c0eea71c286f08defaf3dc8fb
Ednaldo <martinsednaldo@outlook.com>
2020-09-21 17:04:16 -0300

    Usando os Layouts padrão e de autenticação.

--- src/route/RouteWrapper.js
+++ src/route/RouteWrapper.js
@@ -2,12 +2,15 @@ import React from 'react'
 import {Route, Redirect} from 'react-router-dom'
 import PropTypes from 'prop-types'
 
+import AuthLayout from '../page/_layout/auth'
+import DefaultLayout from '../page/_layout/default'
+
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
-  const signed = false
+  const signed = true
 
   if(!signed && isPrivate) {
     return <Redirect to="/" />
@@ -17,7 +20,17 @@ export default function RouteWrapper({
     return <Redirect to="/dashboard" />
   }
 
-  return <Route {...rest} component={Component} />
+  const Layout = signed ? DefaultLayout : AuthLayout
+
+  return (
+    <Route {...rest}
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  )
 }
 
 RouteWrapper.propTypes = {
@@ -31,3 +44,38 @@ RouteWrapper.propTypes = {
 RouteWrapper.defaultProps = {
   isPrivate: false,
 }
+
+
+/*
+
+import React from 'react';
+import PropTypes from 'prop-types'
+
+import {Wrapper} from './style'
+
+export default function AuthLayout({children}) {
+  return (
+    <Wrapper>
+      {children}
+    </Wrapper>
+  );
+}
+
+AuthLayout.propTypes = {
+  children: PropTypes.element.isRequired,
+}
+
+
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  height: 100%;
+  background: linear-gradient(-90deg, #ff9800, #c66900);
+`
+
+/*
+color Primary: #ff9800
+color Dark: #c66900
+color Light: #ffc947
+*/
+
