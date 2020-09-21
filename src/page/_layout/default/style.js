270c5475d7d8d72474ff2885a7342d7bce4796a9
Ednaldo <martinsednaldo@outlook.com>
2020-09-21 17:03:46 -0300

    Layout padrão.

---
+++ src/page/_layout/default/style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  height: 100%;
+  background: linear-gradient(-90deg, #ffc947, #ff9800);
+`
+
+/*
+color Primary: #ff9800
+color Dark: #c66900
+color Light: #ffc947
+*/
