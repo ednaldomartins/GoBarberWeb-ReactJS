fb8a44a8890a8e489df5341a3041041fd17b5e70
Ednaldo <martinsednaldo@outlook.com>
2020-09-22 11:04:17 -0300

    Estilização das páginas de login e cadastro.

--- src/page/_layout/auth/style.js
+++ src/page/_layout/auth/style.js
@@ -1,12 +1,64 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   height: 100%;
-  background: linear-gradient(-90deg, #ff9800, #c66900);
-`
+  background: linear-gradient(180deg, #ff8a65, #c75b39);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
+export const Content = styled.div`
+  width: 100%;
+  max-width: 256px;
+  text-align: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 32px;
+
+    input {
+      background: rgba(0, 0, 0, 0.2);
+      border: 0;
+      border-radius: 4px;
+      height: 32px;
+      padding: 0 16px;
+      color: #fff;
+      margin: 0 0 8px;
+    }
+
+    button {
+      margin: 8px 0 0;
+      height: 32px;
+      background: #00e5ff;
+      color: #000;
+      font-weight: bold;
+      border: 0;
+      border-radius: 4px;
+      transition: background 0.25s;
+
+      &:hover {
+        background: #00b2cc;
+      }
+    }
+
+    a {
+      color: #000;
+      margin-top: 16px;
+      opacity: 0.75;
+
+      &:hover {
+        opacity: 1;
+      }
+    }
+  }
+`;
 /*
-color Primary: #ff9800
-color Dark: #c66900
-color Light: #ffc947
+Primary: #ff9800
+Dark: #c66900
+Light: #ffc947
+Accent-P: #2979ff
+Accent-D: #00b2cc
+Accent-L: #6effff
 */
