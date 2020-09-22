c0b83d2e9baa0f471d048b4a6dac3acba5a5ad3f
Ednaldo <martinsednaldo@outlook.com>
2020-09-22 16:46:00 -0300

    Implementação da método @POST/sessions.

--- src/store/module/auth/reducer.js
+++ src/store/module/auth/reducer.js
@@ -1,5 +1,18 @@
-export default function auth(state = {}, action) {
+import produce from 'immer';
+
+const INITIAL_STATE = {
+  token: null,
+  signed: false,
+  loading: false,
+};
+
+export default function auth(state = INITIAL_STATE, action) {
   switch (action.type) {
+    case '@auth/SIGN_IN_SUCCESS':
+      return produce(state, (draft) => {
+        draft.token = action.payload.token;
+        draft.signed = true;
+      });
     default:
       return state;
   }
--- src/store/module/auth/saga.js
+++ src/store/module/auth/saga.js
@@ -1,3 +1,28 @@
-import { all } from 'redux-saga/effects';
+import { all, takeLatest, call, put } from 'redux-saga/effects';
 
-export default all([]);
+import api from '../../../service/api';
+import history from '../../../service/history';
+
+import { signInSuccess } from './action';
+
+export function* signIn({ payload }) {
+  const { email, password } = payload;
+  const res = yield call(api.post, 'sessions', {
+    email,
+    password,
+  });
+
+  //  se o post der sucesso, entao a resposta tera dados do login
+  const { token, user } = res.data;
+
+  if (!user.provider) {
+    console.tron.error('usuário não prestador de serviço');
+    return;
+  }
+
+  yield put(signInSuccess(token, user));
+
+  history.push('/dashboard');
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
