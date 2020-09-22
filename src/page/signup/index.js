14fbdba5236361a235e9f256f847722a979e3b88
Ednaldo <martinsednaldo@outlook.com>
2020-09-22 11:03:55 -0300

    Criação da página de cadastro.

--- src/page/signup/index.js
+++ src/page/signup/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '../../assets/logo.svg';
 
 export default function SignUp() {
-    return (
-        <div>
-            SignUp
-        </div>
-    );
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input placeholder="Digite aqui seu nome" />
+        <input type="email" placeholder="Digite aqui seu e-mail" />
+        <input type="password" placeholder="Digite aqui sua senha" />
+        <button type="submit">Cadastrar</button>
+        <Link to="/">Já tenho uma conta</Link>
+      </form>
+    </>
+  );
 }
