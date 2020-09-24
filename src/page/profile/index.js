908b53a24667849ad6fef9aee6e29b9357558dc4
Ednaldo <martinsednaldo@outlook.com>
2020-09-24 11:19:10 -0300

    Tela de perfil do usuário.

--- src/page/profile/index.js
+++ src/page/profile/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+
+import { Container } from './style';
 
 export default function Profile() {
-    return (
-        <div>
-            Profile
-        </div>
-    );
+  return (
+    <Container>
+      <Form>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" placeholder="Endereço de e-mail" />
+        <hr />
+        <Input type="password" name="oldPassword" placeholder="Senha antiga" />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação de senha"
+        />
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
