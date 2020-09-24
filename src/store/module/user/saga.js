9fde718f0e19376661bea4ae3573b0bc458888f0
Ednaldo <martinsednaldo@outlook.com>
2020-09-24 12:41:53 -0300

    Ação de atualização de perfil implementada.
    A alteração de senha ainda não está funcionando.

--- src/store/module/user/saga.js
+++ src/store/module/user/saga.js
@@ -1,3 +1,25 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, all, put, call } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-export default all([]);
+import api from '../../../service/api';
+import { updateProfileFailure, updateProfileSuccess } from './action';
+
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, ...rest } = payload.data;
+    const profile = Object.assign(
+      {name, email},
+      rest.oldPassword ? rest : {});
+    const res = yield call(api.put, 'users', profile);
+
+    toast.success('Perfil atualizado com sucesso');
+    yield put(updateProfileSuccess(res.data));
+  } catch (err) {
+    toast.error(
+      'Erro ao tentar atualizar seu perfil. Verifique os dados informados.'
+    );
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
